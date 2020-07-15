0241dda920afef2c8f79b11df64a76f2fa08c57a
Benedictus Utama <bendwiutama@gmail.com>
2020-07-14 19:36:45 -0500

    updating branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react'
+import Header from './Components/Header'
 import Footer from './Components/Footer'
 import Box from './Components/Box'
 
 
 function App() {
+  
+  const [activeTag, setActiveTag] = useState("");
+  const tabs = ['About', 'Resources', 'Events', 'Subsidiaries', 'Contact'];
+  let text = '';
+
+  const name = "Resume Reviews"
+  const blurb = `Bring your resumes and get feedback and advice from upper classmen and an invited experienced 
+  professional who has extensive experience reviewing and evaluating resumes. We will discuss 
+  resume content and formatting tips and provide constructive criticism in an informal setting.  
+  A resume review session is usually held during the week before the university career expo.` 
+
   return (
     <div>
-      <Box/>
+      <Header 
+        activeTag={activeTag}
+        setActiveTag={setActiveTag}
+        tabs={tabs}
+        text={text}
+      />
+      <Box title = {name} desc = {blurb}/>
       <Footer/>
     </div>
   );
