00440128c35b192af4d8186b8785f02149d19204
Benedictus Utama <bendwiutama@gmail.com>
2020-08-07 18:45:44 -0500

    gall prototype 1

--- src/Pages/Home/HomePage.js
+++ src/Pages/Home/HomePage.js
@@ -6,6 +6,12 @@ import graphic from '../../Files/graphic1.jpg'
 import blue_gradient from '../../Files/gradient2.svg'
 import Subsidaries from './Home_Components/Subsidaries'
 
+import AliceCarousel from 'react-alice-carousel'
+import 'react-alice-carousel/lib/alice-carousel.css'
+
+import luay from '../../Files/Home_Gallery/12976851_1138128249563543_7955453846841172046_o.jpg'
+import tim from '../../Files/Home_Gallery/84334897_2881793055197045_101138358037643264_o.jpg'
+import beach from '../../Files/Home_Gallery/116396755_1957654227698199_1890081075763795255_n.jpg'
 
 
 const head = "RICE UNIVERSITY";
@@ -14,7 +20,22 @@ and friends and partner with companies
 to provide the best resources for students to 
 explore computer science and software`;
 
+
+
 function HomePage() {
+
+    let imgs = [luay, tim, beach]
+    let gall = []
+    
+
+    for(let i = 0; i < imgs.length; i++){
+      gall.push(
+      <div>
+        <img src = {imgs[i]} className = "sliderimg"/>
+      </div>
+      )
+    }
+
     return (
     <div className="relative">
       <img class="flex right-0 w-60 h-60 absolute mr-32" src={graphic}></img>
@@ -24,6 +45,11 @@ function HomePage() {
           <p class="text-gray-500b space-y-1 text-base">{first_blurb}</p>
       </div>
       <img class="absolute object-left" width="200px" height="150px" src={triangles}/>
+
+      <div class = "relative h-32">
+        <AliceCarousel items = {gall} autoPlay={true} duration = {3000} mouseTrackingEnabled playButtonEnabled={true} />
+        yee
+      </div>
       
       <Subsidaries/>
       
