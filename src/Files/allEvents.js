4a1184c6e92837b0291e7ad18b21774eb17df607
Benedictus Utama <bendwiutama@gmail.com>
2020-08-24 19:04:08 -0500

    events update aug24 removed filler events

--- src/Files/allEvents.js
+++ src/Files/allEvents.js
@@ -35,37 +35,4 @@ export default [
         host a study break or Christmas party after classes end.`,
         "link": `LINK NOT READY`,
     },
-    {
-        "title": "Late Night Event with a Very Long Name 4",
-        "month": "OCT",
-        "day": "31",
-        "time": "7:30 pm",
-        "locale": "Duncan Hall",
-        "desc": `Throughout the year, we hold social events such as lunches, 
-        study breaks, mixers, bowling nights, and random outings. We regularly 
-        host a study break or Christmas party after classes end.`,
-        "link": `Facebook link`,
-    },
-    {
-        "title": "Late Night Event with a Very Long Name 5",
-        "month": "OCT",
-        "day": "31",
-        "time": "7:30 pm",
-        "locale": "Duncan Hall",
-        "desc": `Throughout the year, we hold social events such as lunches, 
-        study breaks, mixers, bowling nights, and random outings. We regularly 
-        host a study break or Christmas party after classes end.`,
-        "link": `Facebook link`,
-    },
-    {
-        "title": "Late Night Event with a Very Long Name 6",
-        "month": "OCT",
-        "day": "31",
-        "time": "7:30 pm",
-        "locale": "Duncan Hall",
-        "desc": `Throughout the year, we hold social events such as lunches, 
-        study breaks, mixers, bowling nights, and random outings. We regularly 
-        host a study break or Christmas party after classes end.`,
-        "link": `Facebook link`,
-    },
 ]
