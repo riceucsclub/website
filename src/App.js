8cb716b5227a5cacbdb0c8a7062f960ef7e4afb7
Benedictus Utama <bendwiutama@gmail.com>
2021-03-21 20:11:15 -0500

    readme removed and basename should work

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
         <title>{"Rice CS Club"}</title>
       </Helmet>
 
-      <Router basename={website}>
+      <Router basename={'website'}>
         <Switch>
           <Route exact path="/">
           <HomePage/>
