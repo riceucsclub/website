07013cfd94795187decc5b609df5895b7000ba46
theoschweizer <schweizer.theodore@gmail.com>
2021-07-26 11:21:39 -0700

    changed subsidiaries to stack in smaller screens

--- src/Pages/Home/Home_Components/Subsidaries.js
+++ src/Pages/Home/Home_Components/Subsidaries.js
@@ -15,24 +15,24 @@ function Subsidaries () {
     return (
         <div className="">
             <h1 className="text-4xl py-16 px-24 text-gray-500b font-bold">We work with...</h1>
-            <div class="mt-8 grid col-gap-40 grid-cols-10 ">
-                <div class="inline-block col-start-2 col-span-2 text-gray-700 text-center">
+            <div class="flex flex-col md:flex-row justify-center">
+                <div class="flex mx-8 text-gray-700 text-center">
                     <a href="http://remixcs.rice.edu/" target="_blank">
-                        <img src={remixcs_logo} alt="Logo" />
+                        <img src={remixcs_logo} alt="Logo" width="200px"/>
                     </a>
                 </div>
-                <div class="inline-block col-span-2 text-gray-700 text-center">
+                <div class="flex mx-8 text-gray-700 text-center">
                     <a href="https://hack.rice.edu/" target="_blank">
-                        <img src={hackrice_logo} alt="Logo" />
+                        <img src={hackrice_logo} alt="Logo" width="200px"/>
                     </a>
                 </div>
-                <div class="inline-block col-span-2 text-gray-700 text-center">
+                <div class="flex mx-8 text-gray-700 text-center">
                     <a href="http://riceapps.org/" target="_blank">
-                        <img src={riceapps_logo} alt="Logo" />
+                        <img src={riceapps_logo} alt="Logo" width="200px"/>
                     </a>
                 </div>
-                <NavLink exact to="/csio" target="_blank" class="inline-block col-span-2">
-                    <img src={csio_logo} alt="Logo"/>
+                <NavLink exact to="/csio" target="_blank" class="flex mx-8">
+                    <img src={csio_logo} alt="Logo" width="200px"/>
                 </NavLink>
             </div>
         </div>
