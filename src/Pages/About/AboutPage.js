fdd369d7e4ae5921acf62a9e8712ea5b5ac216bb
baylee-rl <bjk9@rice.edu>
2021-10-25 17:09:47 -0500

    updated constitution link

--- src/Pages/About/AboutPage.js
+++ src/Pages/About/AboutPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Footer from '../../Components/Footer'
 import Header from '../../Components/Header'
 // import blue_gradient from '../../Files/blue_gradient.svg'
@@ -10,28 +10,28 @@ import OfficerGallery from './About_Components/OfficerGallery'
 
 function AboutPage() {
 
-  const [activeTag, setActiveTag] = useState("");
-  const tabs = ['About', 'Resources', 'Events', 'Subsidiaries', 'Contact'];
-  let text = '';
+  // const [activeTag, setActiveTag] = useState("");
+  // const tabs = ['About', 'Resources', 'Events', 'Subsidiaries', 'Contact'];
+  // let text = '';
 
   return (
     <div className="relative">
-      <Header/>
+      <Header />
 
       <div className="relative z-10 text-rice-blue text-center font-bold text-4xl lg:text-5xl laptop-l:text-6xl pt-100px mb-6">Welcome to the Rice CS Club!</div>
       <div class="flex flex-col md:flex-row justify-center">
-        <a className="flex justify-center py-1 m-2" href= 'https://docs.google.com/document/d/1EEig4JVePLBKu0MTp7aa5qNrWEQGOorgcm3M4XtXUew/edit' target="_blank">
+        <a className="flex justify-center py-1 m-2" href='https://docs.google.com/document/d/15nBECgAnR3ZiHaNqSZV-4NDMksp5aBd6aceaykslgeI/edit?usp=sharing' target="_blank">
           <button class="bg-rice-blue hover:bg-gull-gray text-white py-2 px-4 rounded-full">View our constitution here!</button>
         </a>
-        <a className="flex justify-center py-1 m-2" href= 'http://eepurl.com/gBMAdr' target="_blank">
+        <a className="flex justify-center py-1 m-2" href='http://eepurl.com/gBMAdr' target="_blank">
           <button class="bg-rice-blue hover:bg-gull-gray text-white py-2 px-4 rounded-full">Join our mailing list here!</button>
         </a>
       </div>
       <h1 className="flex text-gray-500b justify-center text-2xl mt-8 font-semibold justify-start">2020-2021 Officers</h1>
-      <div class = "relative mb-8 z-10 justify-center">
-        <OfficerGallery/>
+      <div class="relative mb-8 z-10 justify-center">
+        <OfficerGallery />
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
