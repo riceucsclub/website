c24475556d217a8d0292b5d1c33666dc08b54330
Quang Nguyen <nnquang.1412@gmail.com>
2021-10-24 20:20:42 -0500

    .

--- src/Pages/Events/Events_Components/Calendar.js
+++ src/Pages/Events/Events_Components/Calendar.js
@@ -1,46 +1,37 @@
-import React from 'react'
-import FutureEvent from './FutureEvent'
-import allEvents from '../../../Files/allEvents'
-
-
-
-
+import React from "react";
+import FutureEvent from "./FutureEvent";
+import allEvents from "../../../Files/allEvents";
 
 function Calender() {
+  let eventlist = [];
 
+  // Moved the google calendar draw to the allEvents.js page in Files
 
-    let eventlist = []
+  //imports json with all future events and makes event objects from each entry
 
-    // Moved the google calendar draw to the allEvents.js page in Files
-            
-    //imports json with all future events and makes event objects from each entry
+  for (let i = 0; i < allEvents.length; i++) {
+    console.log(allEvents[i]);
+    eventlist.push(
+      <FutureEvent
+        title={allEvents[i]["title"]}
+        month={allEvents[i]["month"]}
+        day={allEvents[i]["day"]}
+        time={allEvents[i]["time"]}
+        locale={allEvents[i]["locale"]}
+        desc={allEvents[i]["desc"]}
+        link={allEvents[i]["link"]}
+      />
+    );
+  }
 
-    for(let i = 0; i < allEvents.length; i++){
-        console.log(allEvents[i])
-        eventlist.push(
-            <FutureEvent 
-                title = {allEvents[i]["title"]}
-                month = {allEvents[i]["month"]}
-                day = {allEvents[i]["day"]}
-                time = {allEvents[i]["time"]}
-                locale = {allEvents[i]["locale"]}
-                desc = {allEvents[i]["desc"]}
-                link = {allEvents[i]["link"]}
-            />
-        )
-    }
-
-
-    return (
-        <div>
-            <subheading class = 'font-semibold text-2xl py-2 md:mx-32 xl:mx-64'>
-                Upcoming Events
-            </subheading>
-            <events>
-                {eventlist}
-            </events>
-        </div>
-    )
+  return (
+    <div>
+      <subheading class="font-semibold text-2xl py-2 md:mx-32 xl:mx-64">
+        Upcoming Events
+      </subheading>
+      <events>{eventlist}</events>
+    </div>
+  );
 }
 
-export default Calender
+export default Calender;
