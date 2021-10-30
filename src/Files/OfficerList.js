55e237efcec9c55bf02a40c590feccea59399f91
Quang Nguyen <nnquang.1412@gmail.com>
2021-10-30 09:54:30 -0500

    add about page

--- src/Files/OfficerList.js
+++ src/Files/OfficerList.js
@@ -1,179 +1,192 @@
-import adam from '../Files/officer_pics/adam.jpg'
-import adrienne from '../Files/officer_pics/adrienne.jpg'
-import alp from '../Files/officer_pics/alp.jpeg'
-import angie from '../Files/officer_pics/angie.jpeg'
-import benny from '../Files/officer_pics/benny.jpg'
-import clairew from '../Files/officer_pics/clairew.jpeg'
-import clairex from '../Files/officer_pics/clairex.jpeg'
-import emma from '../Files/officer_pics/emma.jpeg'
-import emily from '../Files/officer_pics/emily.jpeg'
-import jacob from '../Files/officer_pics/jacob.jpeg'
-import kelly from '../Files/officer_pics/kelly.jpeg'
-import luis from '../Files/officer_pics/luis.jpeg'
-import manaal from '../Files/officer_pics/manaal.jpeg'
-import megan from '../Files/officer_pics/megan.jpeg'
-import oeishi from '../Files/officer_pics/oeishi.jpg'
-import paul from '../Files/officer_pics/paul.jpeg'
-import sarah from '../Files/officer_pics/sarah.jpg'
-import shaquille from '../Files/officer_pics/shaquille.jpeg'
-import thomas from '../Files/officer_pics/thomas.jpeg'
-import will from '../Files/officer_pics/will.jpeg'
+import adam from "../Files/officer_pics/adam.jpg";
+import adrienne from "../Files/officer_pics/adrienne.jpg";
+import alp from "../Files/officer_pics/alp.jpeg";
+import angie from "../Files/officer_pics/angie.jpeg";
+import benny from "../Files/officer_pics/benny.jpg";
+import clairew from "../Files/officer_pics/clairew.jpeg";
+import clairex from "../Files/officer_pics/clairex.jpeg";
+import emma from "../Files/officer_pics/emma.jpeg";
+import emily from "../Files/officer_pics/emily.jpeg";
+import jacob from "../Files/officer_pics/jacob.jpeg";
+import kelly from "../Files/officer_pics/kelly.jpeg";
+import luis from "../Files/officer_pics/luis.jpeg";
+import manaal from "../Files/officer_pics/manaal.jpeg";
+import megan from "../Files/officer_pics/megan.jpeg";
+import oeishi from "../Files/officer_pics/oeishi.jpg";
+import paul from "../Files/officer_pics/paul.jpeg";
+import shaquille from "../Files/officer_pics/shaquille.jpeg";
+import thomas from "../Files/officer_pics/thomas.jpeg";
+import will from "../Files/officer_pics/will.jpeg";
+import victor from "../Files/officer_pics/victor.jpeg";
+import eliot from "../Files/officer_pics/eliot.png";
+import sarah from "../Files/officer_pics/sarah.png";
+import quang from "../Files/officer_pics/quang.jpeg";
+import sanjay from "../Files/officer_pics/sanjay.jpeg";
+import eddie from "../Files/officer_pics/eddie.jpeg";
+import lucia from "../Files/officer_pics/lucia.jpeg";
+import ryan from "../Files/officer_pics/ryan.jpeg";
+import shourya from "../Files/officer_pics/shourya.jpeg";
+import linh from "../Files/officer_pics/linh.jpeg";
+import vivian from "../Files/officer_pics/vivian.jpeg";
 
 //template for buffer spots
-    // {
-    //     "image": "",
-    //     "name": "",
-    //     "title": "",
-    //     "college": "",
-    //     "hometown": ""
-    // },
-
+// {
+//     "image": "",
+//     "name": "",
+//     "title": "",
+//     "college": "",
+//     "hometown": ""
+// },
 
 export default [
-    //multi-line strings should be encapuslated with the ` character, same key as the ~ (tilde)
-    //hometown is major. we should change that    
-    {
-        "image": luis,
-        "name": "Luis Clague",
-        "title": "Co-President",
-        "college": "Brown 2021",
-        "hometown": "COMP"
-    },
-    {
-        "image": oeishi,
-        "name": "Oeishi Banerjee",
-        "title": "Co-President",
-        "college": "Sid Rich 2021",
-        "hometown": "COMP, PSYC"
-    },
-    {
-        "image": paul,
-        "name": "Paul Jiang",
-        "title": "Co-Internal VP",
-        "college": "Martel 2021",
-        "hometown": "COMP, FCAM"
-    },
-    {
-        "image": shaquille,
-        "name": "Shaquille Que",
-        "title": "Co-Internal VP",
-        "college": "McMurtry 2022",
-        "hometown": "COMP, MATH"
-    },
-    {
-        "image": kelly,
-        "name": "Kelly Huang",
-        "title": "Co-External VP",
-        "college": "Lovett 2023",
-        "hometown": "COMP, BIOC"
-    },
-    {
-        "image": sarah,
-        "name": "Sarah Han",
-        "title": "Co-External VP",
-        "college": "Martel 2023",
-        "hometown": "COMP, STAT"
-    },
-    {
-        "image": adam,
-        "name": "Adam Zawierucha",
-        "title": "Co-Treasurer",
-        "college": "Brown 2023",
-        "hometown": "COMP"
-    },
-    {
-        "image": adrienne,
-        "name": "Adrienne Li",
-        "title": "Co-Treasurer",
-        "college": "Brown 2023",
-        "hometown": "COMP, ECON"
-    },
+  //multi-line strings should be encapuslated with the ` character, same key as the ~ (tilde)
+  //hometown is major. we should change that
+  {
+    image: emma,
+    name: "Emma Dunn",
+    title: "Co-President",
+    college: "Brown 2021",
+    hometown: "COMP",
+  },
+  {
+    image: alp,
+    name: "Berk Alp Yakici",
+    title: "Co-President",
+    college: "Sid Rich 2021",
+    hometown: "COMP, PSYC",
+  },
+  {
+    image: jacob,
+    name: "Jacob Pan",
+    title: "Co-Internal VP",
+    college: "Martel 2024",
+    hometown: "COMP",
+  },
+  {
+    image: eliot,
+    name: "Eliot Solomon",
+    title: "Co-Internal VP",
+    college: "McMurtry 2023",
+    hometown: "COMP",
+  },
+  {
+    image: kelly,
+    name: "Kelly Huang",
+    title: "Co-External VP",
+    college: "Lovett 2023",
+    hometown: "COMP, BIOC",
+  },
+  {
+    image: sarah,
+    name: "Sarah Han",
+    title: "Co-External VP",
+    college: "Martel 2023",
+    hometown: "COMP, STAT",
+  },
+  {
+    image: sanjay,
+    name: "Sanjay Rajasekhar",
+    title: "Treasurer",
+    college: "Jones 2024",
+    hometown: "COMP, DSCI",
+  },
 
-    {
-        "image": clairex,
-        "name": "Claire Xu",
-        "title": "Co-Events Chair",
-        "college": "Wiess 2023",
-        "hometown": "COMP, BUSI"
-    },
-    {
-        "image": thomas,
-        "name": "Thomas Yuem",
-        "title": "Co-Events Chair",
-        "college": "Wiess 2024",
-        "hometown": "COMP, DSCI"
-    },
-    {
-        "image": manaal,
-        "name": "Manaal Khan",
-        "title": "Co-Webmaster",
-        "college": "Sid Rich 2023",
-        "hometown": "COMP, DSCI"
-    },
-    {
-        "image": benny,
-        "name": "Benedictus Utama",
-        "title": "Co-Webmaster",
-        "college": "Will Rice 2022",
-        "hometown": "COMP, DSCI"
-    },
-    {
-        "image": megan,
-        "name": "Megan Xiao",
-        "title": "Publicist",
-        "college": "Duncan 2023",
-        "hometown": "COMP, NEUR"
-    },
-    {
-        "image": emily,
-        "name": "Emily Huang",
-        "title": "Co-Freshman Rep",
-        "college": "McMurty 2024",
-        "hometown": "COMP, ECON"
-    },
-    {
-        "image": jacob,
-        "name": "Jacob Pan",
-        "title": "Co-Freshman Rep",
-        "college": "Martel 2024",
-        "hometown": "COMP"
-    },
-    {
-        "image": clairew,
-        "name": "Claire Wang",
-        "title": "HackRice Co-Director",
-        "college": "McMurtry 2022",
-        "hometown": "CAAM"
-    },
-    {
-        "image": emma,
-        "name": "Emma Dunn",
-        "title": "HackRice Co-Director",
-        "college": "Wiess 2022",
-        "hometown": "COMP"
-    },
-    {
-        "image": will,
-        "name": "Will Mundy",
-        "title": "Rice Apps Chair",
-        "college": "Sid Rich 2021",
-        "hometown": "COMP, ASIA, FCAM"
-    },
-    {
-        "image": alp,
-        "name": "Berk Alp Yakici",
-        "title": "CS I/O Committee Chair",
-        "college": "Hanszen 2022",
-        "hometown": "COMP, DSCI"
-    },
-    {
-        "image": angie,
-        "name": "Angie Chen",
-        "title": "RemixCS Chair",
-        "college": "Martel 2022",
-        "hometown": "COMP"
-    },
-    
-    
-    
-]
+  {
+    image: emily,
+    name: "Emily Huang",
+    title: "Co-Events Chair",
+    college: "Wiess 2023",
+    hometown: "COMP, BUSI",
+  },
+  {
+    image: megan,
+    name: "Megan Xiao",
+    title: "Co-Events Chair",
+    college: "Duncan 2023",
+    hometown: "COMP, NEUR",
+  },
+  // {
+  //   image: baylee,
+  //   name: "Baylee Keevan",
+  //   title: "Co-Webmaster",
+  //   college: "Baker 2024",
+  //   hometown: "COMP, DSCI",
+  // },
+  {
+    image: quang,
+    name: "Quang Nguyen",
+    title: "Co-Webmaster",
+    college: "Jones 2024",
+    hometown: "COMP, DSCI",
+  },
+  {
+    image: thomas,
+    name: "Newton Huynh",
+    title: "Publicist",
+    college: "Duncan 2022",
+    hometown: "COMP",
+  },
+  {
+    image: vivian,
+    name: "Vivian Xiao",
+    title: "Co-Freshman Rep",
+    college: "Sid 2025",
+    hometown: "COMP",
+  },
+  {
+    image: victor,
+    name: "Victor Xie",
+    title: "Co-Freshman Rep",
+    college: "Lovett 2025",
+    hometown: "COMP",
+  },
+  {
+    image: eddie,
+    name: "Eddie Xiong",
+    title: "HackRice Co-Director",
+    college: "McMurtry 2022",
+    hometown: "CAAM",
+  },
+  {
+    image: ryan,
+    name: "Ryan Draper",
+    title: "HackRice Co-Director",
+    college: "Lovett 2022",
+    hometown: "COMP",
+  },
+  {
+    image: lucia,
+    name: "Lucia Lu",
+    title: "HackRice Co-Director",
+    college: "Wiess 2022",
+    hometown: "COMP",
+  },
+  {
+    image: adam,
+    name: "Adam Zawierucha",
+    title: "Rice Apps Chair",
+    college: "Brown 2023",
+    hometown: "COMP, MATH",
+  },
+  {
+    image: shourya,
+    name: "Shourya Munjal",
+    title: "CS I/O Committee Co-Chair",
+    college: "Brown 2024",
+    hometown: "COMP",
+  },
+  {
+    image: linh,
+    name: "Linh Nguyen",
+    title: "CS I/O Committee Co-Chair",
+    college: "Jones 2023",
+    hometown: "COMP",
+  },
+  {
+    image: angie,
+    name: "Angie Chen",
+    title: "RemixCS Chair",
+    college: "Martel 2022",
+    hometown: "COMP, Entrepreneurship",
+  },
+];
--- src/Pages/CSIO/CSIO_Page.js
+++ src/Pages/CSIO/CSIO_Page.js
@@ -1,51 +1,63 @@
-import React, { useState } from 'react';
-import tan_rect from '../../Files/tan_rect.svg';
-import luay from '../../Files/luay.svg';
-import profile_pic from '../../Files/empty_profile.png';
-import Top_Part from './CSIO_Components/Top_Part'
+import React, { useState } from "react";
+import tan_rect from "../../Files/tan_rect.svg";
+import luay from "../../Files/luay.svg";
+import profile_pic from "../../Files/empty_profile.png";
+import Top_Part from "./CSIO_Components/Top_Part";
 
+import { NavLink } from "react-router-dom";
 
-import {
-  NavLink
-} from 'react-router-dom';
+const CSIO_Page = () => {
+  let maintext =
+    "The CS I/O committee is a small group of undergraduate students involved in computer science with various backgrounds that work to maintain an open line of communication between the Rice student body and the Computer Science Department. Our goal is to create a direct, accessible, and constructive conduit for Rice students to voice their opinions and ideas about Rice Computer Science and for the Computer Science Department to consult initiatives with the student body. As we work with the Computer Science Department, we aim to be both problem and solution-focused (i.e. not a collective rant space for students to target specific professors). In addition to voicing student opinion, we also highlight specific things students think the department does well and continue working with the Department to maintain successful initiatives.";
+  let secondtext =
+    "Some events we host are Ask Luay Anything, Research Panel, and Faculty Feud.";
+  let thirdtext =
+    "If you have suggestions, please reach out to one of our committee members via their email address listed on this page, or fill out the Google form using the link below.";
+  let link_gf = "https://forms.gle/b4mk1Zp4PqkGe2Tx8";
 
-function CSIO_Page() {
-    let maintext= "The CS I/O committee is a small group of undergraduate students involved in computer science with various backgrounds that work to maintain an open line of communication between the Rice student body and the Computer Science Department. Our goal is to create a direct, accessible, and constructive conduit for Rice students to voice their opinions and ideas about Rice Computer Science and for the Computer Science Department to consult initiatives with the student body. As we work with the Computer Science Department, we aim to be both problem and solution-focused (i.e. not a collective rant space for students to target specific professors). In addition to voicing student opinion, we also highlight specific things students think the department does well and continue working with the Department to maintain successful initiatives.";
-    let secondtext= "Some events we host are Ask Luay Anything, Research Panel, and Faculty Feud.";
-    let thirdtext= "If you have suggestions, please reach out to one of our committee members via their email address listed on this page, or fill out the Google form using the link below.";
-    let link_gf = "https://forms.gle/b4mk1Zp4PqkGe2Tx8"
+  return (
+    <div className="bg-black">
+      <Top_Part />
 
-    
-    return (
-      <div className="bg-black">
-        <Top_Part/>
-      
       <div className="relative z-10 bg-black">
-
-          <div className="relative z-10 text-4xl bg-white font-bold">
-              <div className="text-4xl ml-24 py-4 bg-white font-bold">Our Mission</div>
+        <div className="relative z-10 text-4xl bg-white font-bold">
+          <div className="text-4xl ml-24 py-4 bg-white font-bold">
+            Our Mission
           </div>
+        </div>
 
-          <div className="relative z-20 bg-black">
-              <div className="flex justify-center text-center bg-black mx-24 mt-24 text-white">{maintext}</div>
-              <div className="flex justify-center text-center mx-24 mt-8 bg-black text-white">{secondtext}</div>
-              <div className="flex justify-center text-center mx-24 mt-8 bg-black text-white">{thirdtext}</div>
-              <a className="flex justify-center text-center mx-24 mt-2 bg-black text-white" href= 'https://forms.gle/b4mk1Zp4PqkGe2Tx8' target="_blank">
-                {link_gf}
-              </a>
-              {/* <div className="flex justify-center text-center mx-24 mt-2 bg-black text-white" onClick={()=> window.open("https://forms.gle/b4mk1Zp4PqkGe2Tx8")}>{link_gf}</div> */}
+        <div className="relative z-20 bg-black">
+          <div className="flex justify-center text-center bg-black mx-24 mt-24 text-white">
+            {maintext}
           </div>
+          <div className="flex justify-center text-center mx-24 mt-8 bg-black text-white">
+            {secondtext}
+          </div>
+          <div className="flex justify-center text-center mx-24 mt-8 bg-black text-white">
+            {thirdtext}
+          </div>
+          <a
+            className="flex justify-center text-center mx-24 mt-2 bg-black text-white"
+            href="https://forms.gle/b4mk1Zp4PqkGe2Tx8"
+            target="_blank"
+          >
+            {link_gf}
+          </a>
+          {/* <div className="flex justify-center text-center mx-24 mt-2 bg-black text-white" onClick={()=> window.open("https://forms.gle/b4mk1Zp4PqkGe2Tx8")}>{link_gf}</div> */}
+        </div>
 
-          <div className="relative z-10 text-4xl mt-24 bg-white font-bold">
-            <div className="text-4xl py-4 ml-24 bg-white font-bold">Meet the team</div>
+        <div className="relative z-10 text-4xl mt-24 bg-white font-bold">
+          <div className="text-4xl py-4 ml-24 bg-white font-bold">
+            Meet the team
           </div>
+        </div>
 
-          <div className="relative z-10 flex justify-center text-2xl py-6 text-white">
-            Coming Soon!
-          </div>
-          </div>
+        <div className="relative z-10 flex justify-center text-2xl py-6 text-white">
+          Coming Soon!
+        </div>
+      </div>
 
-          {/* <div className="flex mt-8 grid justify-center grid-cols-2">
+      {/* <div className="flex mt-8 grid justify-center grid-cols-2">
               <div className="flex w-2/3 flex-wrap center justify-center">
               <div className="px-4 justify-center align-middle">
                   <img className="flex items-center py-3 justify-center max-w-full h-auto border-none" src={luay}/>
@@ -62,7 +74,7 @@ function CSIO_Page() {
               </div>
           </div> */}
 
-          {/* <div className="flex mt-8 grid justify-center grid-cols-6">
+      {/* <div className="flex mt-8 grid justify-center grid-cols-6">
               <div className="flex w-2/3 flex-wrap center justify-center">
               <div className="px-4 justify-center align-middle">
                   <img className="flex items-center py-3 justify-center max-w-full h-auto border-none" src={profile_pic}/>
@@ -108,8 +120,8 @@ function CSIO_Page() {
           </div> */}
 
       <footer className="text-center py-4 text-white">&copy; CS IO 2020</footer>
-      </div>
-    );
-  }
-  
-  export default CSIO_Page;
+    </div>
+  );
+};
+
+export default CSIO_Page;
