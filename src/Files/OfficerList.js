434676b873363198e93984b0ee9c81bccb0abcdd
joy <jy92@rice.edu>
2023-01-23 09:22:36 -0600

    updated officers

--- src/Files/OfficerList.js
+++ src/Files/OfficerList.js
@@ -1,24 +1,15 @@
-import adam from "../Files/officer_pics/adam.jpg";
-import alp from "../Files/officer_pics/alp.jpg";
-import angie from "../Files/officer_pics/angie.jpeg";
-import baylee from "../Files/officer_pics/baylee.png";
-import eddie from "../Files/officer_pics/eddie.jpeg";
-import eliot from "../Files/officer_pics/eliot.jpg";
-import emma from "../Files/officer_pics/emma.jpeg";
-import emily from "../Files/officer_pics/emily.jpeg";
-import jacob from "../Files/officer_pics/jacob.jpeg";
-import kelly from "../Files/officer_pics/kelly.jpeg";
-import linh from "../Files/officer_pics/linh.jpeg";
-import lucia from "../Files/officer_pics/lucia.jpeg";
-import megan from "../Files/officer_pics/megan.jpeg";
-import quang from "../Files/officer_pics/quang.jpeg";
-import ryan from "../Files/officer_pics/ryan.jpeg";
-import sanjay from "../Files/officer_pics/sanjay.jpeg";
-import sarah from "../Files/officer_pics/sarah.png";
-import shourya from "../Files/officer_pics/shourya.jpeg";
-import victor from "../Files/officer_pics/victor.jpeg";
-import vivian from "../Files/officer_pics/vivian.jpeg";
-import newton from "../Files/officer_pics/newton.jpeg";
+import sarah from "../Files/officer_pics/sarah_han.jpg"
+import eliot from "../Files/officer_pics/eliot_solomon.jpg"
+import jacob from "../Files/officer_pics/jacob_pan.jpg"
+import april from "../Files/officer_pics/april_tong.jpg"
+import ben from "../Files/officer_pics/ben_liu.jpg"
+import brandon from "../Files/officer_pics/brandon_rice.jpg"
+import daniel from "../Files/officer_pics/daniel_liang.jpg"
+import emily from "../Files/officer_pics/emily_huang.jpg"
+import megan from "../Files/officer_pics/megan_xiao.jpg"
+import victor from "../Files/officer_pics/victor_xie.jpg"
+import vivian from "../Files/officer_pics/vivian_xiao.jpg"
+//import joy from 
 
 //template for buffer spots
 // {
@@ -26,25 +17,25 @@ import newton from "../Files/officer_pics/newton.jpeg";
 //     "name": "",
 //     "title": "",
 //     "college": "",
-//     "hometown": ""
+//     "hometown": ""3
 // },
 
 export default [
   //multi-line strings should be encapuslated with the ` character, same key as the ~ (tilde)
   //hometown is major. we should change that
   {
-    image: emma,
-    name: "Emma Dunn",
+    image: sarah,
+    name: "Sarah Han",
     title: "Co-President",
-    college: "Wiess 2022",
-    hometown: "COMP, STAT",
+    college: "Wiess 2023",
+    hometown: "COMP, DSCI",
   },
   {
-    image: alp,
-    name: "Berk Alp Yakici",
+    image: eliot,
+    name: "Eliot Solomon",
     title: "Co-President",
-    college: "Hanszen 2022",
-    hometown: "COMP, DSCI",
+    college: "McMurtry 2023",
+    hometown: "COMP",
   },
   {
     image: jacob,
@@ -54,130 +45,67 @@ export default [
     hometown: "COMP",
   },
   {
-    image: eliot,
-    name: "Eliot Solomon",
+    image: daniel,
+    name: "Daniel Liang",
     title: "Co-Internal VP",
-    college: "McMurtry 2023",
+    college: "?",
     hometown: "COMP",
   },
   {
-    image: kelly,
-    name: "Kelly Huang",
-    title: "Co-External VP",
-    college: "Lovett 2023",
-    hometown: "COMP, BIOC",
-  },
-  {
-    image: sarah,
-    name: "Sarah Han",
-    title: "Co-External VP",
-    college: "Wiess 2023",
-    hometown: "COMP, DSCI",
-  },
-  {
-    image: sanjay,
-    name: "Sanjay Rajasekhar",
-    title: "Treasurer",
-    college: "Jones 2024",
-    hometown: "COMP, DSCI",
-  },
-
-  {
     image: emily,
     name: "Emily Huang",
-    title: "Co-Events Chair",
-    college: "McMurtry 2024",
+    title: "Co-External VP",
+    college: "?",
     hometown: "COMP",
   },
   {
     image: megan,
     name: "Megan Xiao",
-    title: "Co-Events Chair",
+    title: "Co-External VP",
     college: "Duncan 2023",
-    hometown: "COMP, NEUR",
-  },
-  {
-    image: baylee,
-    name: "Baylee Keevan",
-    title: "Co-Webmaster",
-    college: "Baker 2024",
     hometown: "COMP",
   },
   {
-    image: quang,
-    name: "Quang Nguyen",
-    title: "Co-Webmaster",
-    college: "Jones 2024",
-    hometown: "COMP, DSCI",
-  },
-  {
-    image: newton,
-    name: "Newton Huynh",
-    title: "Publicist",
-    college: "Will Rice 2023",
+    image: brandon,
+    name: "Brandon Rice",
+    title: "Treasurer",
+    college: "?",
     hometown: "COMP",
   },
+
   {
     image: vivian,
     name: "Vivian Xiao",
-    title: "Co-Freshman Rep",
+    title: "Co-Events Chair",
     college: "Sid 2025",
     hometown: "COMP",
   },
   {
     image: victor,
     name: "Victor Xie",
-    title: "Co-Freshman Rep",
+    title: "Co-Events Chair",
     college: "Lovett 2025",
     hometown: "COMP",
   },
+//   {
+//     image: joy,
+//     name: "Joy Yu",
+//     title: "Publicist",
+//     college: "Sid 2025",
+//     hometown: "COMP",
+//   },
   {
-    image: eddie,
-    name: "Eddie Xiong",
-    title: "HackRice Co-Director",
-    college: "Jones 2023",
-    hometown: "MATH",
-  },
-  {
-    image: ryan,
-    name: "Ryan Draper",
-    title: "HackRice Co-Director",
-    college: "Lovett 2023",
+    image: april,
+    name: "April Tong",
+    title: "Co-Freshman Rep",
+    college: "? 2026",
     hometown: "COMP",
   },
   {
-    image: lucia,
-    name: "Lucia Lu",
-    title: "HackRice Co-Director",
-    college: "Jones 2023",
+    image: ben,
+    name: "Ben Liu",
+    title: "Co-Freshman Rep",
+    college: "? 2026",
     hometown: "COMP",
-  },
-  {
-    image: adam,
-    name: "Adam Zawierucha",
-    title: "Rice Apps Chair",
-    college: "Brown 2023",
-    hometown: "COMP, MATH",
-  },
-  {
-    image: shourya,
-    name: "Shourya Munjal",
-    title: "CS I/O Committee Co-Chair",
-    college: "Brown 2024",
-    hometown: "COMP",
-  },
-  {
-    image: linh,
-    name: "Linh Nguyen",
-    title: "CS I/O Committee Co-Chair",
-    college: "Jones 2023",
-    hometown: "COMP",
-  },
-  {
-    image: angie,
-    name: "Angie Chen",
-    title: "RemixCS Chair",
-    college: "Martel 2022",
-    hometown: "COMP, ENTR",
   },
 ];
