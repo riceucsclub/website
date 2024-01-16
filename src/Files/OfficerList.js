cadbf886e1c4d64892edf9623d814a51f0f2e7f6
annanguyen04 <an63@rice.edu>
2024-01-16 14:40:32 -0600

    isabelle again

--- src/Files/OfficerList.js
+++ src/Files/OfficerList.js
@@ -7,7 +7,7 @@ import victor from "../Files/officer_pics/officers_2023-2024/victor.jpeg"
 import vivian from "../Files/officer_pics/officers_2023-2024/vivian.jpeg"
 import joy from "../Files/officer_pics/officers_2023-2024/joy.jpg"
 import keshav from "../Files/officer_pics/officers_2023-2024/keshav.jpg"
-import isabelle1 from "..Files/officer_pics/officers_2023-2024/IMG_8112 - Isabelle Goodman.JPG"
+import isabelle from "..Files/officer_pics/officers_2023-2024/isabelle.JPG"
 import gavin from "../Files/officer_pics/officers_2023-2024/gavin.jpeg"
 import sophia from "../Files/officer_pics/officers_2023-2024/sophia.jpg"
 import anna_nguyen from "../Files/officer_pics/officers_2023-2024/anna.jpg"
@@ -82,7 +82,7 @@ export default [
     major: "COMP",
   },
   {
-    image: isabelle1,
+    image: isabelle,
     name: "Isabelle Goodman",
     title: "Co-Events Chair",
     college: "Will Rice 2025",
@@ -120,7 +120,7 @@ export default [
     image: gavin,
     name: "Gavin Ni",
     title: "Co-Freshman Rep",
-    college: "Hanszen 2027",
+    college: "Brown 2027",
     major: "COMP, BUSI",
   }
 
