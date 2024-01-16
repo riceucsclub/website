f982162790dee09c42f4328aba1dcc1b2e605164
annanguyen04 <an63@rice.edu>
2024-01-16 15:29:17 -0600

    try to add calendar

--- src/Pages/Events/Events_Components/Calendar.js
+++ src/Pages/Events/Events_Components/Calendar.js
@@ -2,41 +2,55 @@ import React from "react";
 import FutureEvent from "./FutureEvent";
 import allEvents from "../../../Files/allEvents";
 
-function Calender() {
+function Calendar() {
   let eventlist = [];
 
   // Moved the google calendar draw to the allEvents.js page in Files
 
   //imports json with all future events and makes event objects from each entry
-  // if (allEvents.length > 0) {
-  //   for (let i = 0; i < allEvents.length; i++) {
-  //     console.log(allEvents[i]);
-  //     eventlist.push(
-  //       <FutureEvent
-  //         title={allEvents[i]["title"]}
-  //         month={allEvents[i]["month"]}
-  //         day={allEvents[i]["day"]}
-  //         time={allEvents[i]["time"]}
-  //         locale={allEvents[i]["locale"]}
-  //         desc={allEvents[i]["desc"]}
-  //         link={allEvents[i]["link"]}
-  //       />
-  //     );
-  //   }
-  // } else {
-  //   eventlist.push(<h1 class="flex flex-row my-4 mx-1/6 md:mx-36 xl:mx-72">No events currently scheduled... Check back later!</h1>)
-  // }
+  if (allEvents.length > 0) {
+    for (let i = 0; i < allEvents.length; i++) {
+      console.log(allEvents[i]);
+      eventlist.push(
+        <FutureEvent
+          title={allEvents[i]["title"]}
+          month={allEvents[i]["month"]}
+          day={allEvents[i]["day"]}
+          time={allEvents[i]["time"]}
+          locale={allEvents[i]["locale"]}
+          desc={allEvents[i]["desc"]}
+          link={allEvents[i]["link"]}
+        />
+      );
+    }
+  } else {
+    eventlist.push(
+      <h1 className="flex flex-row my-4 mx-1/6 md:mx-36 xl:mx-72">
+        No events currently scheduled... Check back later!
+      </h1>
+    );
+  }
 
-  
+  const googleCalendarCode = `
+    <iframe
+      src="https://calendar.google.com/calendar/embed?src=o10val6sb8gbqt74v3tle2deeg%40group.calendar.google.com&ctz=America%2FChicago"
+      style="border: 0"
+      width="800"
+      height="600"
+      frameborder="0"
+      scrolling="no"
+    ></iframe>
+  `;
 
   return (
     <div>
-      <subheading class="font-semibold text-2xl py-2 md:mx-32 xl:mx-64">
+      <subheading className="font-semibold text-2xl py-2 md:mx-32 xl:mx-64">
         Upcoming Events
       </subheading>
-      <iframe src="https://calendar.google.com/calendar/embed?src=o10val6sb8gbqt74v3tle2deeg%40group.calendar.google.com&ctz=America%2FChicago" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+      <div dangerouslySetInnerHTML={{ __html: googleCalendarCode }} />
+      {eventlist}
     </div>
   );
 }
 
-export default Calender;
+export default Calendar;
